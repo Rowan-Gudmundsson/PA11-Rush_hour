1f421c94ca63be29956b8b7a23f5fce969453b05
Spencer Kase <34220995+NTreeP@users.noreply.github.com>
2017-12-03 14:54:52 -0800

    Add files via upload

--- Rush.cpp
+++ Rush.cpp
@@ -1,1 +1,42 @@
-﻿
+#include <iostream>
+#include "Rush.h"
+#include <map>
+
+using namespace std;
+
+Board::Board():totalCars(0){}
+Board::~Board(){
+	delete [] boards;
+	boards = NULL;
+}
+
+void Board::insert(int cars){
+	totalCars = cars;
+	boards = new Vehicle [cars];
+	for(int i = 0; i < cars; i++){
+		cin >> boards[i].length
+			>> boards[i].direction
+			>> boards[i].row
+			>> boards[i].col;
+	}
+}
+
+int Board::solveIt(int cars){
+	Board board;
+	board.insert(cars);
+	board.showStructure();
+}
+
+void Board::clear(){
+	totalCars = 0;
+}
+
+void Board::showStructure() const{
+	for(int i = 0; i < totalCars; i++){
+		cout << boards[i].length << " "
+		<< boards[i].direction << " "
+		<< boards[i].row << " "
+		<< boards[i].col << " "
+		<< endl;
+	}
+}
--- Rush.h
+++ Rush.h
@@ -1,1 +1,38 @@
-﻿
+#ifndef RUSH_H
+#define RUSH_H
+
+using namespace std;
+
+
+class Board{
+public:
+	Board();
+	~Board();
+
+	void clear();
+	void insert(int cars);
+	int solveIt(int cars);
+	bool isSolved() const;
+	void showStructure() const;
+
+private:
+	class Vehicle{
+	public:
+		int row;
+		int col;
+		char direction;
+		int length;
+		char name;
+	};
+	Vehicle *boards;
+	int totalCars;
+	/*
+	bool moveForward(Board &board, Vehicle test);
+	bool moveBackward(Board &board, Vehicle test);
+	void solveIt(Board &board,int &cap, int moves, bool &solved);
+	bool win(Board board);
+	Vehicle array[6][6];
+	*/
+};
+
+#endif
